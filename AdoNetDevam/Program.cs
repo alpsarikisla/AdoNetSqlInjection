5b2a24602312c9ec44364c09680393d8b3a2f8a7
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-01-09 17:50:34 +0300

    Uygulama Yazıldı

--- AdoNetDevam/Program.cs
+++ AdoNetDevam/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace AdoNetDevam
     {
         static void Main(string[] args)
         {
+            #region Kategori Numarası yazılan ürünleri listeleyin
+
+
+            Console.WriteLine("Ürünlerinin Listelenmesini istediğiniz kategori numarasını yazınız");
+            int katNo = Convert.ToInt32(Console.ReadLine());
+
+            SqlConnection con = new SqlConnection(@"Data Source = DESKTOP-6VC48K2; Initial Catalog = NORTHWND; Integrated Security=True");
+            SqlCommand cmd = con.CreateCommand();
+
+            cmd.CommandText = "SELECT ProductID,ProductName,UnitPrice FROM Products WHERE CategoryID = " + katNo;
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string isim = reader.GetString(1);
+                decimal fiyat = reader.GetDecimal(2);
+                Console.WriteLine($"{id}) {isim} {fiyat}");
+            }
+            con.Close();
+
+            #endregion
         }
     }
 }
